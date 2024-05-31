073a877d6d2a09a3d082549b22286a3061c23002
Ömer Birler <omer.birler@outlook.com>
2024-05-31 17:36:26 +0300

    Add project files.

---
+++ OpenTKArcballReplicate/ArcballCamera.cs
@@ -0,0 +1,183 @@
+﻿using System;
+
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace OpenTKArcballReplicate
+{
+    /* A simple arcball camera that moves around the camera's focal point.
+    * The mouse inputs to the camera should be in normalized device coordinates,
+    * where the top-left of the screen corresponds to [-1, 1], and the bottom
+    * right is [1, -1].
+    */
+    public class ArcballCamera
+    {
+        /* Create an arcball camera focused on some center point
+     * screen: [win_width, win_height]
+     */
+        public ArcballCamera(Vector3 eye, Vector3 center, Vector3 up, int width, int height)
+        {
+            _width = width;
+            _height = height;
+            Vector3 dir = center - eye;
+            Vector3 z_axis = Vector3.Normalize(dir);
+            Vector3 x_axis = Vector3.Normalize(Vector3.Cross(z_axis, up));
+            Vector3 y_axis = Vector3.Normalize(Vector3.Cross(x_axis, z_axis));
+            x_axis = Vector3.Normalize(Vector3.Cross(z_axis, y_axis));
+
+            center_translation = Matrix4.CreateTranslation(center).Inverted();
+            translation = Matrix4.CreateTranslation(0, 0, -dir.Length);
+
+            // Create the initial rotation quaternion from the axis vectors
+            rotation = Quaternion.FromMatrix(new Matrix3(
+                x_axis.X, y_axis.X, -z_axis.X,
+                x_axis.Y, y_axis.Y, -z_axis.Y,
+                x_axis.Z, y_axis.Z, -z_axis.Z
+            ));
+
+            float aspect = (float)_width / ((float)_height);
+
+            set_projection(fov, aspect, near, far);
+
+            update_camera();
+        }
+
+        private int _width;
+
+        private int _height;
+
+        // We store the unmodified look at matrix along with
+        // decomposed translation and rotation components
+        private Matrix4 center_translation;
+        private Matrix4 translation;
+        private Quaternion rotation = Quaternion.Identity;
+
+        // camera is the full camera transform,
+        // inv_camera is stored as well to easily compute
+        // eye position and world space rotation axes
+
+        private Matrix4 camera;
+        private Matrix4 inv_camera;
+
+        private Matrix4 proj;
+
+        private Matrix4 proj_inv;
+
+        private float fov = 65f;
+
+        private float near = 0.1f;
+
+        private float far = 100.0f;
+
+        public void set_projection(float fov, float aspect, float near, float far)
+        {
+            proj = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(fov), aspect, near, far);
+            proj_inv = proj.Inverted();
+        }
+
+        public void resize(int width, int height)
+        {
+            _width = width;
+            _height = height;
+            float aspect = (float)_width / ((float)_height);
+            set_projection(fov, aspect, near, far);
+        }
+
+        public void rotate(Vector2 prev_mouse, Vector2 cur_mouse)
+        {
+            // Clamp mouse positions to stay in NDC
+            cur_mouse = Vector2.Clamp(cur_mouse, new Vector2(-1, -1), new Vector2(1, 1));
+            prev_mouse = Vector2.Clamp(prev_mouse, new Vector2(-1, -1), new Vector2(1, 1));
+
+            // Convert screen positions to arcball positions
+            Quaternion mouse_cur_ball = screen_to_arcball(cur_mouse);
+            Quaternion mouse_prev_ball = screen_to_arcball(prev_mouse);
+
+            // Update rotation
+            rotation = mouse_cur_ball * mouse_prev_ball * rotation;
+            update_camera();
+        }
+
+        public void pan(Vector2 mouse_delta)
+        {
+            float zoom_amount = Math.Abs(translation.M32);
+            Vector4 motion = new Vector4(mouse_delta.X * zoom_amount, mouse_delta.Y * zoom_amount, 0f, 0f);
+
+            // Find the panning amount in the world space
+            motion = inv_camera * motion;
+
+            center_translation = Matrix4.CreateTranslation(motion.Xyz) * center_translation;
+            update_camera();
+        }
+
+        public void zoom(float zoom_amount)
+        {
+            Vector3 motion = new Vector3(0f, 0f, zoom_amount);
+            translation = Matrix4.CreateTranslation(motion) * translation;
+            update_camera();
+        }
+
+        public Matrix4 transform()
+        {
+            return camera;
+        }
+
+        public Matrix4 inv_transform()
+        {
+            return inv_camera;
+        }
+
+        public Matrix4 projection()
+        {
+            return proj;
+        }
+
+        public Matrix4 inv_projection()
+        {
+            return proj_inv;
+        }
+
+        public Vector3 eye()
+        {
+            Vector4 eyePos = inv_camera * new Vector4(0, 0, 0, 1);
+            return new Vector3(eyePos.X, eyePos.Y, eyePos.Z);
+        }
+
+        public Vector3 dir()
+        {
+            Vector4 dirPos = inv_camera * new Vector4(0, 0, -1, 0);
+            return new Vector3(dirPos.X, dirPos.Y, dirPos.Z).Normalized();
+        }
+
+        public Vector3 up()
+        {
+            Vector4 upPos = inv_camera * new Vector4(0, 1, 0, 0);
+            return new Vector3(upPos.X, upPos.Y, upPos.Z).Normalized();
+        }
+
+        private void update_camera()
+        {
+            Matrix4 rotationMatrix = Matrix4.CreateFromQuaternion(rotation);
+            camera = translation * rotationMatrix * center_translation;
+            inv_camera = camera.Inverted();
+        }
+
+        private Quaternion screen_to_arcball(Vector2 point)
+        {
+            float dist = Vector2.Dot(point, point);
+
+            // If we're on/in the sphere return the point on it
+            if (dist < 1.0f)
+            {
+                return new Quaternion(0.0f, point.X, point.Y, MathF.Sqrt(1.0f - dist));
+            }
+            else
+            {
+                // otherwise we project the point onto the sphere
+                Vector2 proj = Vector2.Normalize(point);
+                return new Quaternion(0.0f, proj.X, proj.Y, 0.0f);
+            }
+        }
+    }
+}
---
+++ OpenTKArcballReplicate/Program.cs
@@ -0,0 +1,256 @@
+﻿using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.Common;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace OpenTKArcballReplicate
+{
+    public class Game : GameWindow
+    {
+        public Game(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
+            : base(gameWindowSettings, nativeWindowSettings)
+        {
+            VSync = VSyncMode.On;
+            prev_mouse = new Vector2(-2f, -2f);
+            _width = nativeWindowSettings.ClientSize.X;
+            _height = nativeWindowSettings.ClientSize.Y;
+        }
+
+        private static ArcballCamera _camera;
+
+        private static int _width;
+        private static int _height;
+
+        private int _shaderProgram;
+        private int _vertexArray;
+        private int _vertexBuffer;
+        private int _indexBuffer;
+
+        private int _projviewLocation;
+
+        private int _draw_size;
+
+        private bool mouse_left = false;
+
+        private bool mouse_right = false;
+
+        private Vector2 prev_mouse;
+
+        private static DebugProc DebugMessageDelegate = OpenGLDebugMessage;
+
+        private readonly float[] _vertices = {
+            // Front face
+            -1.0f, -1.0f,  1.0f,
+             1.0f, -1.0f,  1.0f,
+             1.0f,  1.0f,  1.0f,
+            -1.0f,  1.0f,  1.0f,
+            // Back face
+            -1.0f, -1.0f, -1.0f,
+             1.0f, -1.0f, -1.0f,
+             1.0f,  1.0f, -1.0f,
+            -1.0f,  1.0f, -1.0f,
+        };
+
+        private readonly uint[] _indices = {
+            // Front face
+            0, 1, 2, 2, 3, 0,
+            // Top face
+            3, 2, 6, 6, 7, 3,
+            // Back face
+            7, 6, 5, 5, 4, 7,
+            // Bottom face
+            4, 5, 1, 1, 0, 4,
+            // Left face
+            4, 0, 3, 3, 7, 4,
+            // Right face
+            1, 5, 6, 6, 2, 1,
+        };
+
+        protected override void OnLoad()
+        {
+            GL.ClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+            GL.Enable(EnableCap.DepthTest);
+
+            GL.DebugMessageCallback(DebugMessageDelegate, IntPtr.Zero);
+            GL.Enable(EnableCap.DebugOutput);
+
+            _shaderProgram = CreateShaderProgram("main.vert", "main.frag");
+            GL.UseProgram(_shaderProgram);
+
+            _vertexArray = GL.GenVertexArray();
+            GL.BindVertexArray(_vertexArray);
+            
+            _vertexBuffer = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBuffer);
+            GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+
+            _indexBuffer = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, _indexBuffer);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, _indices.Length * sizeof(uint), _indices, BufferUsageHint.StaticDraw);
+
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            _projviewLocation = GL.GetUniformLocation(_shaderProgram, "proj_view");
+
+            _camera = new ArcballCamera(new Vector3(0, 0, 5), Vector3.Zero, Vector3.UnitY, _width, _height);
+
+            _draw_size = (int)(_vertices.Length / 3.0);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.UseProgram(_shaderProgram);
+
+            // Use shaders and draw objects here...
+
+            var projection = _camera.projection();
+            var transform = _camera.transform();
+
+            var projview = projection * transform;
+            // Pass transform matrix to shader here...
+
+            GL.UniformMatrix4(_projviewLocation, false, ref projview);
+
+            GL.BindVertexArray(_vertexArray);
+            GL.DrawElements(PrimitiveType.Triangles, _indices.Length, DrawElementsType.UnsignedInt, 0);
+
+            SwapBuffers();
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+            _width = Size.X;
+            _height = Size.Y;
+            GL.Viewport(0, 0, _width, _height);
+            _camera.resize(_width, _height);
+        }
+
+        protected override void OnMouseMove(MouseMoveEventArgs e)
+        {
+            base.OnMouseMove(e);
+
+            Vector2 cur_mouse = transform_mouse(e.X, e.Y);
+
+            if (prev_mouse.X != -2f)
+            {
+                if (mouse_left)
+                {
+                    _camera.rotate(prev_mouse, cur_mouse);
+                }
+                else if(mouse_right)
+                {
+                    Vector2 dxy = cur_mouse - prev_mouse;
+                    Vector4 dxy4 = _camera.inv_projection() * new Vector4(dxy.X, dxy.Y, 0f, 1f);
+
+                    _camera.pan(new Vector2(dxy4.X, dxy4.Y));
+                }
+            }
+            prev_mouse = cur_mouse;
+        }
+
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseDown(e);
+
+            if (e.Button == MouseButton.Left)
+            {
+                mouse_left = true;
+            }
+            else if (e.Button == MouseButton.Right)
+            {
+                mouse_right = true;
+            }
+        }
+
+        protected override void OnMouseUp(MouseButtonEventArgs args)
+        {
+            if (args.Button == MouseButton.Left)
+            {
+                mouse_left = false;
+            }
+            else if (args.Button == MouseButton.Right)
+            {
+                mouse_right = false;
+            }
+        }
+
+        protected override void OnMouseWheel(MouseWheelEventArgs e)
+        {
+            base.OnMouseWheel(e);
+            _camera.zoom(e.OffsetY * 0.1f);
+        }
+
+        private int CreateShaderProgram(string vertexPath, string fragmentPath)
+        {
+            int vertexShader = CompileShader(ShaderType.VertexShader, File.ReadAllText(vertexPath));
+            int fragmentShader = CompileShader(ShaderType.FragmentShader, File.ReadAllText(fragmentPath));
+
+            int shaderProgram = GL.CreateProgram();
+            GL.AttachShader(shaderProgram, vertexShader);
+            GL.AttachShader(shaderProgram, fragmentShader);
+            GL.LinkProgram(shaderProgram);
+
+            GL.GetProgram(shaderProgram, GetProgramParameterName.LinkStatus, out int status);
+            if (status == 0)
+            {
+                throw new Exception(GL.GetProgramInfoLog(shaderProgram));
+            }
+
+            GL.DeleteShader(vertexShader);
+            GL.DeleteShader(fragmentShader);
+
+            return shaderProgram;
+        }
+
+        private int CompileShader(ShaderType type, string source)
+        {
+            int shader = GL.CreateShader(type);
+            GL.ShaderSource(shader, source);
+            GL.CompileShader(shader);
+
+            GL.GetShader(shader, ShaderParameter.CompileStatus, out int status);
+            if (status == 0)
+            {
+                throw new Exception(GL.GetShaderInfoLog(shader));
+            }
+
+            return shader;
+        }
+
+        private Vector2 transform_mouse(float xposIn, float yposIn)
+        {
+            return new Vector2(xposIn * 2f / Size.X - 1f, 1f - 2f * yposIn / Size.X);
+        }
+
+        private static void OpenGLDebugMessage(DebugSource source, DebugType type, int id, DebugSeverity severity, int length, IntPtr pMessage, IntPtr pUserParam)
+        {
+            var message = Marshal.PtrToStringAnsi(pMessage, length);
+            Console.WriteLine($"[{severity} source={source} type={type} id={id}] {message}");
+        }
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var nativeWindowSettings = new NativeWindowSettings()
+            {
+                ClientSize = new Vector2i(800, 600),
+                Title = "LearnOpenTK - Creating a Window",
+                // This is needed to run on macos
+                Flags = ContextFlags.ForwardCompatible,
+            };
+
+            using (Game game = new Game(GameWindowSettings.Default, nativeWindowSettings))
+            {
+                game.Run();
+            }
+        }
+    } 
+}
